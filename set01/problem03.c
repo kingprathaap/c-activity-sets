2c27452867eb0531daafca470153a51c83e15794
kingprathaap <prathaapm.bsc23@rvu.edu.in>
2024-02-23 14:12:53 +0530

    test

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int input();
-int add(int a, int b);
-void output(int a, int b, int sum);
+int add(int c, int d);
+void output(int c, int d, int sum);
 int main()
 {
-  int a,b,sum;
-  a=input();
-  b=input();
-  sum=add(a,b);
-  output(a,b,sum);
+  int c,d,sum;
+  c=input();
+  d=input();
+  sum=add(c,d);
+  output(c,d,sum);
   return 0;
 }
 int input()
@@ -18,12 +18,12 @@ int input()
   scanf("%d",&n);
   return n;
 }
-int add(int a, int b)
+int add(int c, int d)
 {
-  int sum=a+b;
+  int sum=c+d;
   return sum;
 }
-void output(int a, int b, int sum)
+void output(int c, int d, int sum)
 {
- printf("the sum of %d and %d is %d",a,b,sum);
+ printf("the sum of %d and %d is %d",c,d,sum);
 }
