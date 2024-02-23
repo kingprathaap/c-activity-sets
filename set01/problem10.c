f807ac67735780c7810db44d48aa1a107686f966
kingprathaap <prathaapm.bsc23@rvu.edu.in>
2024-02-23 15:27:40 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -15,9 +15,9 @@ int main()
 }
 void input_two_strings(char *string1, char *string2)
 {
-    printf("Enter the first string\n");
+    printf("enter the first string\n");
     scanf("%s",&string1);
-    printf("Enter the second string\n");
+    printf("enter the second string\n");
     scanf("%s",&string2);
 }
 int stringcompare(char *string1, char *string2)
